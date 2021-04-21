61b2414ce80132c3db77ccc0b911eaa382150d66
rogulforce <piotr.r99@gmail.com>
2021-04-21 15:06:24 +0200

    some small changes

--- Python/data_get.py
+++ Python/data_get.py
@@ -1,5 +1,5 @@
 def data_get(filename):
-    with open(file=filename, mode='r', encoding='utf-8'):
-        pass
+    with open(file=filename, mode='r', encoding='utf-8') as f:
+        print(f.read())
     return
 
--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,4 @@
-print(1)
+from data_get import data_get
+
+if __name__ == "__main__":
+    data_get('..\przydatne_linki.txt')
